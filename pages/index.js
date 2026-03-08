a9f7c738b73298acf9ac5350cc84d007bf3aab7e
ibrahimtayyabit <109470001+ibrahimtayyabit@users.noreply.github.com>
2026-03-09 02:25:35 +0500

    checking supabase push

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-console.log('ENV CHECK:', process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
 import { useState, useEffect } from 'react'
 import { supabase } from '../lib/supabase'
 import Head from 'next/head'
