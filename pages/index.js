9616e4258c5804091a0e58c596bbb0196ce5c7ea
ibrahimtayyabit <109470001+ibrahimtayyabit@users.noreply.github.com>
2026-03-09 02:14:41 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+console.log('ENV CHECK:', process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
 import { useState, useEffect } from 'react'
 import { supabase } from '../lib/supabase'
 import Head from 'next/head'
